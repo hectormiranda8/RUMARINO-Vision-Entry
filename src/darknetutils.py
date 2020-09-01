bebaecf4a95893c28c3d7027126f9c74856c9491
Guy S Garcia <34169809+FloppyDisck@users.noreply.github.com>
2020-09-01 13:35:43 -0400

    Update darknetutils.py
    Fixed formatting

--- src/darknetutils.py
+++ src/darknetutils.py
@@ -1,7 +1,7 @@
 import os
 
 def darknet_pretrain(darknet, original_model, original_weights, trained_weights="pretrained-weights.conv.15"):
-    os.system{f"{darknet} partial {original_model} {original_weights} {trained_weights} 15"}
+    os.system(f"{darknet} partial {original_model} {original_weights} {trained_weights} 15")
 
 def darknet_train(darknet, detector, model, trained_weights, log="train.log"):
-    os.system{f"{darknet} partial {detector} {model} {trained_weights} -gpus  0 -map > {log}"}
+    os.system(f"{darknet} partial {detector} {model} {trained_weights} -gpus  0 -map > {log}")
